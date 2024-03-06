45f83df8d808830a6505e467356d9057311c3efa
Yury Cavalcante <yury.cavalcante@cumbuca.com>
2024-03-06 01:05:15 -0300

    added basic logic for chatbot recursive conversation

--- lib/chatbot.ex
+++ lib/chatbot.ex
@@ -4,15 +4,32 @@ defmodule Chatbot do
   """
 
   @doc """
-  Hello world.
+  Start chatbot using this method
 
-  ## Examples
+      Example:
 
-      iex> Chatbot.hello()
-      :world
-
+      Chatbot.start()
   """
-  def hello do
-    :world
+  def start() do
+    input = IO.gets("User: ")
+
+    normalized = normalize_input(input)
+
+    run(normalized)
+  end
+
+  defp normalize_input(input) do
+    input
+    |> String.trim("\n")
+    |> String.downcase()
+  end
+
+  defp run("quit"), do: System.halt(0)
+
+  defp run(input) when is_binary(input) do
+    # TODO implement OpenAI context here
+    IO.puts("Hello :D")
+
+    start()
   end
 end
